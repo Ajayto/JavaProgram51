922e940701f29b30609a65d43770cda7e513ec79
Ajayto <ajaytoradmal2009@gmail.com>
2022-07-01 21:15:24 +0530

    Update git1.java

--- src/AccessModfir2/git1.java
+++ src/AccessModfir2/git1.java
@@ -5,7 +5,7 @@ public class git1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i=0;i<=10;i++) {
+		for(int i=0;i<=30;i++) {
 			System.out.println(i);
 		}
 
